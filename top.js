93071ee5d5ad3691fa49e99d3986e8e9e51956c2
Corey Lawrence <coreyml15@gmail.com>
2024-10-30 17:00:01 -0400

    Animation optimizations

--- top.js
+++ top.js
@@ -6,12 +6,25 @@ document.addEventListener('DOMContentLoaded', function () {
     opacity: 0,
     ease: 'power3.out',
   });
+  gsap.to('.headline', {
+    duration: 1,
+    y: 0,
+    opacity: 1,
+    ease: 'power3.out',
+  });
 
   gsap.from('.headline-line2', {
     duration: 1,
     y: 30,
     opacity: 0,
-    delay: 0.4, // Slightly increased delay to differentiate from line 1
+    delay: 0.4,
+    ease: 'power3.out',
+  });
+  gsap.to('.headline-line2', {
+    duration: 1,
+    y: 0,
+    opacity: 1,
+    delay: 0.4,
     ease: 'power3.out',
   });
 
@@ -21,26 +34,39 @@ document.addEventListener('DOMContentLoaded', function () {
     delay: 0.6,
     ease: 'power3.out',
   });
+  gsap.to('.subheadline', {
+    duration: 1,
+    opacity: 1,
+    delay: 0.6,
+    ease: 'power3.out',
+  });
 
   gsap.from('.hero .cta', {
     duration: 1,
     opacity: 0,
-    delay: 0.9,
+    delay: 0.6,
+    ease: 'power3.out',
+  });
+  gsap.to('.hero .cta', {
+    duration: 1,
+    opacity: 1,
+    delay: 0.6,
     ease: 'power3.out',
   });
 
   // Add animation for the #highlight block
   gsap.from('#highlight', {
-    duration: 1,
-    padding: '0.5em 0em', // Initial padding
+    duration: 0.8,
+    padding: '0.5em 0em',
+    opacity: 0,
     ease: 'power3.out',
-    delay: 0.0, // Adjust the delay as needed to fit your sequence
-    onComplete: () => {
-      gsap.to('#highlight', {
-        padding: '0.5em 2.5em', // Final padding
-        duration: 0.5, // Duration for the padding expansion
-        ease: 'power3.out',
-      });
-    },
+    delay: 0.0,
+  });
+  gsap.to('#highlight', {
+    duration: 0.8,
+    padding: '0.5em 2.5em',
+    opacity: 1,
+    ease: 'power3.out',
+    delay: 1,
   });
 });
